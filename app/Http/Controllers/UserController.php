e2717aa6e883f53964114c085e4158c5296589b5
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-03-27 12:43:39 -0300

    Criação da rota que retorna o QR code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use PHPUnit\Exception;
 
 class UserController extends Controller
 {
     public function store(Request $request)
     {
-        User::create([
-            'name' => $request->name,
-            'linkedin' => $request->linkedin,
-            'github' => $request->github
+        $validate = Validator::make($request->all(),[
+            'name' => 'unique:users|required',
+            'linkedin' => 'required',
+            'github' => 'required'
         ]);
-        return "Usuario criado";
+
+        if ($validate->fails()) {
+            return response()->json(['message' => 'Erro ao gerar codigo QR!', 'error' => $validate->errors()])->setStatusCode(422);
+        }
+
+        try {
+            User::create([
+                'name' => $request->name,
+                'linkedin' => $request->linkedin,
+                'github' => $request->github
+            ]);
+
+            $base_url = "http://localhost:8083/api/" . $request->name;
+
+            $qrCode = QrCode::format('png')->size(250)->generate($base_url);
+            return response()->json(['message' => 'Codigo QR criado', 'qrcode' => $qrCode])->setStatusCode(200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Erro por parte do servidor', 'error' => $e->getMessage()]);
+        }
     }
 }
